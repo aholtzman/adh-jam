4c03fb26f3a35d75070b5120734673b2c9041d8d
Adam Holtzman <adam.holtzman@gmail.com>
2019-12-27 20:52:06 -0500

    fix typo in path name

--- src/pages/undone.js
+++ src/pages/undone.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Page from '../components/galleryPage'
-import data from '../images/undone/data'
+import data from '../images/Undone/data'
 
 export default  () => (
   <Page data={data} title={`Undone - 2003`} />
