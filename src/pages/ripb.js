a3b26518694872a300e54b95619e7290952bf58d
Adam Holtzman <adam.holtzman@gmail.com>
2019-12-28 05:11:50 -0500

    fix title

--- src/pages/ripb.js
+++ src/pages/ripb.js
@@ -3,5 +3,5 @@ import Page from '../components/galleryPage'
 import data from '../images/RIPB/data'
 
 export default  () => (
-  <Page data={data} title={`Rhode Island Pictire Book (or fuck rainbows) - 2015`} />
+  <Page data={data} title={`Rhode Island Picture Book (or fuck rainbows) - 2015`} />
 )
